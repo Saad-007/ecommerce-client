d34e58699f291b714a55bbc7e174c6a39500d83d
Saad-007 <saadsafeer223@gmail.com>
2025-07-24 01:34:26 +0500

    Update API base URL in api.js

--- src/api/api.js
+++ src/api/api.js
@@ -3,13 +3,11 @@ import axios from 'axios';
 
 export const BASE_URL = 'https://e-commerce-backend-production-909a.up.railway.app/api';
 
-
 const API = axios.create({
-  baseURL:BASE_URL,            // Use env variable for deployment, fallback to localhost for dev
-  withCredentials: true, 
+  baseURL: BASE_URL,
+  withCredentials: true,
 });
 
-// Add request interceptor for token
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
   if (token) {
