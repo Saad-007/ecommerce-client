f060d7d63cd9c5333dd5b37a36dbd2c5123b7eca
Saad-007 <saadsafeer223@gmail.com>
2025-07-24 20:20:20 +0500

    Update in vercel.json file and vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: './', // ✅ Fix for correct asset paths on Vercel
+  base: '/', // ✅ Fix for correct asset paths on Vercel
   plugins: [react(), tailwindcss()],
 })
