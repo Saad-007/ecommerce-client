e1c7c13ad8957729a218d4d8a8e196e67c76b81c
Saad-007 <saadsafeer223@gmail.com>
2025-07-24 20:31:11 +0500

    Update vercel.json and vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-
 export default defineConfig({
-  base: '/', // ✅ Fix for correct asset paths on Vercel
+  base: './',
   plugins: [react(), tailwindcss()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://e-commerce-backend-production-36dc.up.railway.app',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  }
 })
