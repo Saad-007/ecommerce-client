d916f88adde3c4c51597fec05e3f3a9207cb385b
Saad-007 <saadsafeer223@gmail.com>
2025-07-25 02:24:11 +0500

    Include loading for user

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,10 +1,11 @@
-// src/components/ProtectedRoute.jsx
 import { useAuth } from '../context/AuthContext';
 import { Navigate, Outlet } from 'react-router-dom';
 
 const ProtectedRoute = ({ adminOnly = false }) => {
-  const { user, isAdmin } = useAuth();
-  if (loading) return null; // or a loader
+  const { user, isAdmin, loading } = useAuth(); // ✅ include loading
+
+  if (loading) return null; // Or show a loading spinner
+
   if (!user) {
     return <Navigate to="/login" replace />;
   }
