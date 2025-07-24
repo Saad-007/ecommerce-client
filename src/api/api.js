7e966a1af5e58295c1ad8252f62390e3dfa8e316
Saad-007 <saadsafeer223@gmail.com>
2025-07-25 02:56:23 +0500

    update the status

--- src/api/api.js
+++ src/api/api.js
@@ -4,7 +4,7 @@ const FRONTEND_URL = import.meta.env.VITE_FRONTEND_URL || 'http://localhost:5173
 
 const API = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
-  withCredentials: true,
+  withCredentials: false,
   headers: {
     'Content-Type': 'application/json',
     'Accept': 'application/json'
